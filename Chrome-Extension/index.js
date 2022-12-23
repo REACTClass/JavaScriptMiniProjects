0179726696ea8c16990b30c189697ccc4663444e
REACTClass <21jamoon@gmail.com>
2022-12-23 01:34:18 -0500

    updated app

--- Chrome-Extension/index.js
+++ Chrome-Extension/index.js
@@ -2,11 +2,26 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
 
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
     inputEl.value = "" // <- clears input field after saving value
+    localStorage.setItem("myLeads", JSON.stringify(myLeads)) //set items in local storage and set them as strings
     renderLeads()
 })
 
